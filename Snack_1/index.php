a3dd422e16d9d501382de1225b88c9101ecb449c
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-30 16:03:50 +0200

    Snack 1 - Coding

--- Snack_1/index.php
+++ Snack_1/index.php
@@ -1,5 +1,46 @@
-<?php 
+<?php
+$firstMatch = [
+    'teamHome' => 'Napoli',
+    'teamAway' => 'Milan',
+    'teamHomeScore' => '4',
+    'teamAwayScore' => '1',
+];
+$secondMatch = [
+    'teamHome' => 'Geona',
+    'teamAway' => 'Sampdoria',
+    'teamHomeScore' => '3',
+    'teamAwayScore' => '0',
+];
+$thirdMatch = [
+    'teamHome' => 'Inter',
+    'teamAway' => 'Torino',
+    'teamHomeScore' => '2',
+    'teamAwayScore' => '2',
+];
+$fourthMatch = [
+    'teamHome' => 'Benevento',
+    'teamAway' => 'Palermo',
+    'teamHomeScore' => '1',
+    'teamAwayScore' => '1',
+];
+$fifthMatch = [
+    'teamHome' => 'Sassuolo',
+    'teamAway' => 'Cesena',
+    'teamHomeScore' => '3',
+    'teamAwayScore' => '0',
+];
+$sixthMatch = [
+    'teamHome' => 'Atalanta',
+    'teamAway' => 'Juventus',
+    'teamHomeScore' => '3',
+    'teamAwayScore' => '2',
+];
 
+// proseguire con ciclo for stasera
+
+
+
+// var_dump($firstMatch);
 ?>
 
 <!DOCTYPE html>
@@ -20,5 +61,34 @@
 </head>
 
 <!-- Snack 1
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Creiamo un array contenente le partite di calcio di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55-60 -->
+
+
+<body>
+    <div class="container f-d-tab fs-5">
+        <div>
+            <p>
+                <?php echo $firstMatch['teamHome']
+
+                    ?>
+            </p>
+            <p>
+                <?php echo $firstMatch['teamHomeScore'] 
+                
+                ?>
+            </p>
+        </div>
+        <div>
+            <p>
+                <?php echo $firstMatch['teamAway']
+
+                    ?>
+            </p>
+            <p>
+                <?php echo $firstMatch['teamAwayScore'] ?>
+            </p>
+        </div>
+    </div>
+
+</body>
